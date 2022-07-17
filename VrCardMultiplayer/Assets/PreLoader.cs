fba0376eb2a6239bbb6f5a897c4c5643b60c6375
TheFakeThanos <EerdekensInc@gmail.com>
2022-07-17 18:00:36 +0200

    added Preloader to test on servers

---
+++ VrCardMultiplayer/Assets/PreLoader.cs
@@ -0,0 +1,36 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PreLoader : MonoBehaviour
+{
+    public NetworkManager network;
+
+    private void Awake()
+    {
+        string[] args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            Debug.Log("ARG " + i + ": " + args[i]);
+            if (args[i] == "-Start-Server")
+            {
+                StartServer();
+            }
+        }
+    }
+
+
+    public void StartHost()
+    {
+        network.StartHost();
+    }
+
+    public void StartServer()
+    {
+        network.StartServer();
+    }
+
+    public void StartClient()
+    {
+        network.StartClient();
+    }
+}
